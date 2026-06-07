54fd0754e616319f97173bf7be1a6b7103c95c39
ivanls <ivan.lacassie@gmail.com>
2026-06-07 21:52:30 +0200

    Delete to-do-list2 directory

--- to-do-list2/src/main/java/com/lakituki/to_do_list2/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lakituki.to_do_list2.repository;
-
-import com.lakituki.to_do_list2.model.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
-}
